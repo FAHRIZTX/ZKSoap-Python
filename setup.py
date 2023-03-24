68433d3faaff697628a3e45475486abd1887cf5d
Muhammad Fahri <fahriztx77@gmail.com>
2023-03-24 18:45:24 +0700

    Update version v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
    name='zksoap',
-   version='0.0.1',
+   version='0.0.2',
    author='Muhammad Fahri',
    author_email='admin+tech@fahriztx.dev',
    long_description = long_description,
