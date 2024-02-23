3cc512080b11780654a7e74d367a78676eb4910b
Muhammad Fahri <fahriztx77@gmail.com>
2024-02-23 11:33:12 +0700

    Update 0.0.6

--- setup.py
+++ setup.py
@@ -5,14 +5,19 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
    name='zksoap',
-   version='0.0.5',
+   version='0.0.6',
    author='Muhammad Fahri',
-   author_email='admin+tech@fahriztx.dev',
+   author_email='admin@fahriztx.dev',
    long_description = long_description,
    long_description_content_type = "text/markdown",
    package_dir = {"": "src"},
    packages = setuptools.find_packages(where="src"),
    python_requires = ">=3.6",
    license='MIT',
-   url='https://github.com/FAHRIZTX/ZKSoap-Python'
+   url='https://fahriztx.dev/',
+   project_urls = {
+    "Bug Tracker": "https://github.com/FAHRIZTX/ZKSoap-Python/issues",
+    "Documentation": "https://github.com/FAHRIZTX/ZKSoap-Python/blob/master/README.md",
+    "Source Code": "https://github.com/FAHRIZTX/ZKSoap-Python",
+   }
 )
